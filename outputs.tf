9cb0f93ed1043f352c8b96a815a2a1729d94dde0
Matheus-Reato <129328254+Matheus-Reato@users.noreply.github.com>
2023-05-15 14:51:28 -0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,3 @@
-output "subnet_a_id" {
-  value = aws_subnet.subnet_a.id
-}
-
-output "subnet_b_id" {
-  value = aws_subnet.subnet_b.id
-}
-
-output "security_group_id" {
-  value = aws_security_group.my_security_group.id
-}
-
-output "aws_vpc_id" {
-  value = "aws_vpc.my_vpc.id"
+output "DNS-LOADBALANCER" {
+  value = aws_lb.my_load_balancer.dns_name
 }
